fbb65479c2c55abaa8e1c68411be6c103ebf54a7
psyc://psyced.org/~lynX <@>
2011-06-01 20:36:23 +0200

    +

--- install.js
+++ install.js
@@ -5,7 +5,7 @@
 // Please tell us what we need to fix to make it work!
 
 var displayName = "CertPatrol";
-var version     = "1.8.3";
+var version     = "1.9beta";
 var name        = "certpatrol"; // leafname of the JAR file
 
 // The following three sets of variables tell this installer script how your
