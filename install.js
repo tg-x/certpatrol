26816caff4c1a9d2f104a8bd2e9c667eb5abe8ca
psyc://psyced.org/~lynX <@>
2011-06-07 16:39:14 +0200

    +viewHierarchy=View Certification Hierarchy

--- install.js
+++ install.js
@@ -5,7 +5,7 @@
 // Please tell us what we need to fix to make it work!
 
 var displayName = "CertPatrol";
-var version     = "1.9beta";
+var version     = "1.9beta2";
 var name        = "certpatrol"; // leafname of the JAR file
 
 // The following three sets of variables tell this installer script how your
