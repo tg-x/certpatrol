f55042316ae491207bf0a7ae2af6f5b1768bb5b0
tg(x) <*@tg-x.net>
2011-06-13 22:27:44 +0200

    added missing prefs

--- defaults/preferences/CertPatrol.js
+++ defaults/preferences/CertPatrol.js
@@ -1,5 +1,7 @@
 pref("certpatrol.popup.new", false);
 pref("certpatrol.popup.change", false);
+pref("certpatrol.popup.wild", false);
+pref("certpatrol.privatebrowsing.save", false);
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.CertPatrol@PSYC.EU.description", "chrome://certpatrol/locale/CertPatrol.properties");
