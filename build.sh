b805a24ac99c6e7ce2ab88b8c0d540c3e549be04
tg(x) <*@tg-x.net>
2011-06-13 11:56:14 +0200

    build: find regex fix

--- build.sh
+++ build.sh
@@ -68,7 +68,7 @@ mkdir --parents --verbose $TMP_DIR
 JAR_FILE=$TMP_DIR/chrome/$APP_NAME.jar
 echo "Generating $JAR_FILE..."
 for CHROME_SUBDIR in $CHROME_PROVIDERS; do
-  find $CHROME_SUBDIR -path '*.svn*' -prune -o -type f -print | grep -v \~ >> files
+  find $CHROME_SUBDIR -regex '.*/\.[^.]+\|.*~$' -prune -o -type f -print >> files
 done
 
 #zip -0 -r $JAR_FILE `cat files`
@@ -79,7 +79,7 @@ cp --verbose --parents `cat files` $TMP_DIR
 echo "Copying various files to $TMP_DIR folder..."
 for DIR in $ROOT_DIRS; do
   mkdir $TMP_DIR/$DIR
-  FILES="`find $DIR -path '*.svn*' -prune -o -type f -print | grep -v \~`"
+  FILES="`find $DIR -regex '.*/\.[^.]+\|.*~$' -prune -o -type f -print`"
   echo $FILES >> files
   cp --verbose --parents $FILES $TMP_DIR
 done
