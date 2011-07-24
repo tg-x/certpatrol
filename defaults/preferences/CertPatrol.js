f26542e1d51788368115108d7461091fb19beebe
tg(x) <*@tg-x.net>
2011-07-24 21:48:12 +0200

    set notify timeout to 10s by default

--- defaults/preferences/CertPatrol.js
+++ defaults/preferences/CertPatrol.js
@@ -2,7 +2,7 @@ pref("certpatrol.popup.new", false);
 pref("certpatrol.popup.change", false);
 pref("certpatrol.popup.wild", false);
 pref("certpatrol.privatebrowsing.save", false);
-pref("certpatrol.notify.timeout", 0);
+pref("certpatrol.notify.timeout", 10);
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.CertPatrol@PSYC.EU.description", "chrome://certpatrol/locale/CertPatrol.properties");
