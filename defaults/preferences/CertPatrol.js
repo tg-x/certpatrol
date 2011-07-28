4b9aa0860a6d01723b458ad27b9fb8e2e3115de4
tg(x) <*@tg-x.net>
2011-07-28 10:40:16 +0200

    added si0.twimg.com to hosts.ignore

--- defaults/preferences/CertPatrol.js
+++ defaults/preferences/CertPatrol.js
@@ -3,7 +3,7 @@ pref("certpatrol.popup.change", false);
 pref("certpatrol.popup.wild", false);
 pref("certpatrol.privatebrowsing.save", false);
 pref("certpatrol.notify.timeout", 10);
-pref("certpatrol.hosts.ignore", "");
+pref("certpatrol.hosts.ignore", "si0.twimg.com");
 
 // See http://kb.mozillazine.org/Localize_extension_descriptions
 pref("extensions.CertPatrol@PSYC.EU.description", "chrome://certpatrol/locale/CertPatrol.properties");
