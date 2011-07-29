0d2c3fbe82a5a5a69dbc647956aafa2b9351c9a6
tg(x) <*@tg-x.net>
2011-07-30 00:40:50 +0200

    added notify.new & notify.wild prefs; reorganized prefs dialog

--- defaults/preferences/CertPatrol.js
+++ defaults/preferences/CertPatrol.js
@@ -2,6 +2,8 @@ pref("certpatrol.popup.new", false);
 pref("certpatrol.popup.change", false);
 pref("certpatrol.popup.wild", false);
 pref("certpatrol.privatebrowsing.save", false);
+pref("certpatrol.notify.new", true);
+pref("certpatrol.notify.wild", true);
 pref("certpatrol.notify.timeout", 10);
 pref("certpatrol.hosts.ignore", "si0.twimg.com");
 
