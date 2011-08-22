487401e11960c023e96ded4f7eb5aec6ed698aa1
tg(x) <*@tg-x.net>
2011-08-22 14:59:54 +0200

    2.0.11rc1

--- install.js
+++ install.js
@@ -7,7 +7,7 @@
 (function() {
 
 var displayName = "CertPatrol";
-var version     = "2.0.10";
+var version     = "2.0.11rc1";
 var name        = "certpatrol"; // leafname of the JAR file
 
 // The following three sets of variables tell this installer script how your
